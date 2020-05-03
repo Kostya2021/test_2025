076a6f5ef4a08a06fbe51087b8df4a5dab54ed22
André Nitze <andre.nitze@th-brandenburg.de>
2020-05-03 08:42:21 +0200

    Change JUnit test to CI

---
+++ test/main/java/de/andrenitze/softpro/GameplayTest.java
@@ -0,0 +1,20 @@
+package main.java.de.andrenitze.softpro;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GameplayTest {
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void testSingleTenderParticipation() {
+        // 1) A game is running with at least one player
+        // 2) A tender is spawned
+        // 3) One player participates in the tender
+        assertTrue(1==1);
+    }
+}
