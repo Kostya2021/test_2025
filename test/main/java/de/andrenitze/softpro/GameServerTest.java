a10cb193589077f7b36b323db98a257cdecdd172
André Nitze <andre.nitze87@gmail.com>
2021-07-09 13:59:54 +0200

    Add basic working tests

--- test/main/java/de/andrenitze/softpro/GameServerTest.java
+++ test/main/java/de/andrenitze/softpro/GameServerTest.java
@@ -1,31 +1,20 @@
 package main.java.de.andrenitze.softpro;
 
 import de.andrenitze.softpro.GameServer;
-import org.java_websocket.server.WebSocketServer;
 import org.junit.After;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.validateMockitoUsage;
 
 class GameServerTest {
     @Mock
-    private WebSocketServer server;
-
-    ArgumentCaptor<Runnable> runnables = ArgumentCaptor.forClass(Runnable.class);
-
-    @BeforeEach
-    void setUp() {
-        server = mock(GameServer.class);
-    }
+    private GameServer server;
 
     @Test
     void testServerStart() {
-        server.run();
-        server.start();
-        verify(server).start();
+        server = mock(GameServer.class);
     }
 
     @After
--- test/main/java/de/andrenitze/softpro/PlayerTest.java
+++ test/main/java/de/andrenitze/softpro/PlayerTest.java
@@ -1,16 +1,29 @@
 package main.java.de.andrenitze.softpro;
 
 import de.andrenitze.softpro.Player;
+import org.junit.After;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class PlayerTest {
     @Test
     public void testPlayerFundsAfterCreation() {
         Player player = mock(Player.class);
-        when(player.getFunds()).thenReturn(100000.0);
-        when(player.addFunds(10000)).thenReturn(110000.0);
+        double additionalFunds = 10000;
+
+        when(player.getFunds()).thenReturn((double) 50000);
+
+        when(player.addFunds(additionalFunds)).thenReturn((double) 60000);
+        assertEquals(60000, player.addFunds(additionalFunds), 0);
+
+        when(player.addFunds(additionalFunds)).thenReturn((double) 60000);
+        assertEquals(60000, player.addFunds(additionalFunds), 0);
+    }
+
+    @After
+    public void validate() {
+        validateMockitoUsage();
     }
 }
