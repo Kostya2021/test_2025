6a474693b98a7c5eacbed7dc7a5a24f5a9ee07b6
André Nitze <andre.nitze87@gmail.com>
2021-12-21 13:31:34 +0100

    Add "occurrence" and "read" field for the story frontend

--- src/main/java/de/andrenitze/softpro/entities/StoryElement.java
+++ src/main/java/de/andrenitze/softpro/entities/StoryElement.java
@@ -2,45 +2,38 @@ package de.andrenitze.softpro.entities;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import de.andrenitze.softpro.types.AvatarType;
 import de.andrenitze.softpro.types.DeliveryMediumType;
 
 public class StoryElement {
-    @JsonProperty
     @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
             property = "id")
     @JsonIdentityReference(alwaysAsId = true)
     private int id;
 
-    @JsonProperty
     private AvatarType avatar;
+    private String[] lines;
+    private boolean sent;
+    private boolean read;
+    private int occurrence;
 
     /**
      * Earliest occurrence of the objective in days (=game ticks).
      * Will not spawn before that day, even if requirements are met.
      */
-    @JsonProperty
-    public int earliestOccurrence = 1;
+    private int earliestOccurrence = 1;
 
     /**
      * Line will be shown after the referenced objective is completed.
      */
-    @JsonProperty
     private int afterObjective;
 
     /**
      * Delivery medium (email | face-to-face | messenger | messenger-group)
      * In none is provided, it's "face-to-face" (avatar will show up in person).
      */
-    @JsonProperty
     private DeliveryMediumType medium = DeliveryMediumType.FACE2FACE;
-
-    @JsonProperty
-    private String[] lines;
-
-    private boolean sent;
 
     public int getEarliestOccurrence() {
         return earliestOccurrence;
