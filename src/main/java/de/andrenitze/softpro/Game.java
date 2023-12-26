1ee50a15c09802119f47bea7abee0457cfa6f5ec
André Nitze <andre.nitze87@gmail.com>
2023-12-26 16:20:43 +0100

    Decrease game speed

--- src/main/java/de/andrenitze/softpro/Game.java
+++ src/main/java/de/andrenitze/softpro/Game.java
@@ -25,7 +25,7 @@ import static java.lang.Math.round;
 import static java.time.LocalDate.now;
 
 public class Game {
-    private static final int GAME_SPEED_IN_MILLISECONDS = 500;
+    private static final int GAME_SPEED_IN_MILLISECONDS = 1000;
     private static final int BANKRUPTCY_THRESHOLD = -25000;
     private static final String EVENT_TYPE = "type";
     public static final float PROJECT_SPAWN_PROBABILITY = 0.1f;
