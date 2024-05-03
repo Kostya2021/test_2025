40897473d02b9b7ee9c50e03229b8306ec4622c6
André Nitze <andre.nitze87@gmail.com>
2024-05-03 07:55:47 +0200

    Small change

--- src/main/java/de/andrenitze/softpro/types/GameOverStatsDAO.java
+++ src/main/java/de/andrenitze/softpro/types/GameOverStatsDAO.java
@@ -8,7 +8,7 @@ import java.sql.*;
 import static de.andrenitze.softpro.Main.logger;
 
 public class GameOverStatsDAO {
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     public GameOverStatsDAO(DataSource dataSource) {
         this.dataSource = dataSource;
