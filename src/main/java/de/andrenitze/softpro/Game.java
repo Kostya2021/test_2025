71af08336f0c47b59777af592063be3735e4f8d1
André Nitze <andre.nitze87@gmail.com>
2024-05-23 07:39:10 +0200

    Make it easier to recover by lowering bankruptcy threshold

--- src/main/java/de/andrenitze/softpro/Game.java
+++ src/main/java/de/andrenitze/softpro/Game.java
@@ -30,7 +30,7 @@ import static java.time.LocalDate.now;
 
 public class Game {
     private static final int GAME_SPEED_IN_MILLISECONDS = 1000;
-    private static final int BANKRUPTCY_THRESHOLD = -25000;
+    private static final int BANKRUPTCY_THRESHOLD = -100000;
     private static final String EVENT_TYPE = "type";
     public static final float PROJECT_SPAWN_PROBABILITY = 0.1f;
     public static final int STALE_TENDERS_KILL_DAYS = 548;
