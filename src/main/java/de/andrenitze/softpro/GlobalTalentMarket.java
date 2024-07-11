8dfac09e239243cfd4ba402bf05e348f6b119739
André Nitze <andre.nitze87@gmail.com>
2024-07-11 14:51:17 +0200

    Fix 'wrong employee hired' issue

--- src/main/java/de/andrenitze/softpro/GlobalTalentMarket.java
+++
@@ -1,9 +0,0 @@
-package de.andrenitze.softpro;
-
-public class GlobalTalentMarket {
-    private static final TalentMarket instance = new TalentMarket(new EmployeeIdGenerator());
-
-    public static TalentMarket getInstance() {
-        return instance;
-    }
-}
