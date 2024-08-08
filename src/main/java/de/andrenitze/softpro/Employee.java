75f6d11737e42e05b48f91a458b2fa3823eab5ff
André Nitze <andre.nitze87@gmail.com>
2024-08-08 16:53:12 +0200

    Add first and last names to employees

--- src/main/java/de/andrenitze/softpro/Employee.java
+++ src/main/java/de/andrenitze/softpro/Employee.java
@@ -1,10 +1,10 @@
 package de.andrenitze.softpro;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import de.andrenitze.softpro.types.ProjectType;
 
 import java.util.EnumMap;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 import static de.andrenitze.softpro.GameServer.RANDOM;
@@ -16,21 +16,27 @@ public class Employee {
     public static final int MAXIMUM_SICK_DAYS = 22;
     private final Integer id;
     private int salary; // monthly salary
+    @JsonProperty
     private int age;
-    private String name;
+    @JsonProperty
+    private String firstName;
+    @JsonProperty
+    private String lastName;
     private final transient HashMap<Project, Integer> projectExperience;
     private final EnumMap<ProjectType, Integer> projectTypeExperience;
     private final HashMap<String, Integer> projectDomainExperience = new HashMap<>();
+    @JsonProperty
     private int satisfaction;
     private int annualSickDays;
     private boolean isSick = false;
     private int lastSickDay = -1;
     private float health = 0.0f;
-    private final String gender;
+    @JsonProperty
+    private String gender;
 
     Employee(Integer id) {
-        this.name = generateName();
-        this.gender = assignGender();
+        generateName();
+        assignGender();
 
         // Randomize salary between 3000 and 4500
         this.salary = new Random().nextInt(0, 1500) + 3000;
@@ -69,18 +75,19 @@ public class Employee {
         }
     }
 
-    private String assignGender() {
+    private void assignGender() {
         if (RANDOM.nextFloat() <= 0.5) {
-            return "male";
+            this.gender = "male";
         } else {
-            return "female";
+            this.gender = "female";
         }
     }
 
-    private String generateName() {
+    private void generateName() {
         String[] firstNames = {"Rahul","Siddharth","Rutuja","Neelam","Khushi","Ramanan","Pratik","Prashant","Arusha","Sasashy","Tanya","Priyanka","Deepak","Mahesh","Manoj","Naveen","Radhika","Krishna","Nishant","Priya","Adam","Alex","Aaron","Ben","Carl","Dan","David","Edward","Fred","Frank","George","Hal","Hank","Ike","John","Jack","Joe","Larry","Monte","Matthew","Mark","Nathan","Otto","Paul","Peter","Roger","Roger","Steve","Thomas","Tim","Ty","Victor","Walter","Olivia","Emma","Charlotte","Amelia","Ava","Sophia","Isabella","Mia","Evelyn","Harper","Luna","Camila","Gianna","Elizabeth","Eleanor","Ella","Abigail","Sofia","Avery","Scarlett","Emily","Aria","Penelope","Chloe","Layla","Mila","Nora","Hazel","Madison","Ellie","Lily","Nova","Isla","Grace","Violet","Aurora","Riley","Zoey","Willow","Emilia","Stella","Zoe","Victoria","Hannah","Addison","Leah","Lucy","Eliana","Ivy","Everly","Lillian","Paisley","Elena","Naomi","Maya","Natalie","Kinsley","Delilah","Claire","Audrey","Aaliyah","Ruby","Brooklyn","Alice","Aubrey","Autumn","Leilani","Savannah","Valentina","Kennedy","Madelyn","Josephine","Bella","Skylar","Genesis","Sophie","Hailey","Sadie","Natalia","Quinn","Caroline","Allison","Gabriella","Anna","Serenity","Nevaeh","Cora","Ariana","Emery","Lydia","Jade","Sarah","Eva","Adeline","Madeline","Piper","Rylee","Athena","Peyton","Everleigh"};
         String[] lastNames = {"Agarwal","Khatri","Ahuja","Anand","Patel","Babu","Balakrishnan","Banerjee","Varma","Dara","Chakrabarti","Deshpande","Gupta","Shah","Parekh","Singh","Modi","Acharya","Anderson","Ashwoon","Aikin","Bateman","Bongard","Bowers","Boyd","Cannon","Cast","Deitz","Dewalt","Ebner","Frick","Hancock","Haworth","Hesch","Hoffman","Kassing","Knutson","Lawless","Lawicki","Mccord","McCormack","Miller","Myers","Nugent","Ortiz","Orwig","Ory","Paiser","Pak","Pettigrew","Quinn","Quizoz","Ramachandran","Resnick","Sagar","Schickowski","Schiebel","Sellon","Severson","Shaffer","Solberg","Soloman","Sonderling","Soukup","Soulis","Stahl","Sweeney","Tandy","Trebil","Trusela","Trussel","Turco","Uddin","Uflan","Ulrich","Upson","Vader","Vail","Valente","Van Zandt","Vanderpoel","Ventotla","Vogal","Wagle","Wagner","Wakefield","Weinstein","Weiss","Woo","Yang","Yates","Yocum","Zeaser","Zeller","Ziegler","Bauer","Baxster","Casal","Cataldi","Caswell","Celedon","Chambers","Chapman","Christensen","Darnell","Davidson","Davis","DeLorenzo","Dinkins","Doran","Dugelman","Dugan","Duffman","Eastman","Ferro","Ferry","Fletcher","Fietzer","Hylan","Hydinger","Illingsworth","Ingram","Irwin","Jagtap","Jenson","Johnson","Johnsen","Jones","Jurgenson","Kalleg","Kaskel","Keller","Leisinger","LePage","Lewis","Linde","Lulloff","Maki","Martin","McGinnis","Mills","Moody","Moore","Napier","Nelson","Norquist","Nuttle","Olson","Ostrander","Reamer","Reardon","Reyes","Rice","Ripka","Roberts","Rogers","Root","Sandstrom","Sawyer","Schlicht","Schmitt","Schwager","Schutz","Schuster","Tapia","Thompson","Tiernan","Tisler" };
-        return firstNames[new Random().nextInt(firstNames.length)] + " " + lastNames[(new Random().nextInt(firstNames.length))];
+        this.firstName = firstNames[new Random().nextInt(firstNames.length)];
+        this.lastName = lastNames[(new Random().nextInt(firstNames.length))];
     }
 
     int getId() {
@@ -91,27 +98,12 @@ public class Employee {
         return salary;
     }
 
-    int getProjectExperience() {
-        int experience = 0;
-        if (projectExperience != null) {
-            for (Map.Entry<Project, Integer> entry : projectExperience.entrySet()) {
-                Integer experiencePerProject = entry.getValue();
-                experience += experiencePerProject;
-            }
-        }
-        return experience;
-    }
-
     Integer getExperienceInDaysByProject(Project project) {
         Integer experience = 0;
         if (projectExperience.get(project) != null) {
             experience = projectExperience.get(project);
         }
         return experience;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void gainExperience(Project project, Integer newExperienceInDays) {
@@ -210,10 +202,6 @@ public class Employee {
         projectTypeExperience.put(type, existingExperience + days);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setSalary(int i) {
         this.salary = i;
 
@@ -252,5 +240,17 @@ public class Employee {
 
     public void setAge(int i) {
         this.age = i;
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
