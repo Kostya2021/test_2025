ef0aea7ea1e61d6a0ab2a4a42b93d7adb2568011
André Nitze <andre.nitze87@gmail.com>
2024-12-11 18:06:08 +0100

    Remove skill points from testing

--- src/main/java/de/andrenitze/softpro/Player.java
+++ src/main/java/de/andrenitze/softpro/Player.java
@@ -60,7 +60,7 @@ public class Player {
 
     // Skill points are acquired after gaining a certain amount of XP and are invested to unlock skills
     @Getter @Setter
-    private int skillPoints = 4;
+    private int skillPoints = 0;
 
     // The level the player has reached in the game
     @Setter @Getter
