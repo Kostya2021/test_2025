2214c978c6029b1a168f3c579cd502b9b486def2
André Nitze <andre.nitze87@gmail.com>
2025-04-25 14:03:24 +0200

    Externalize DB config

--- src/main/java/de/andrenitze/softpro/Main.java
+++ src/main/java/de/andrenitze/softpro/Main.java
@@ -1,37 +1,30 @@
 package de.andrenitze.softpro;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.io.IOException;
-import java.util.Properties;
 
 @SpringBootApplication
 @Slf4j
 public class Main {
     private static final int CONNECTION_LOST_TIMEOUT = 5;
 
+    @Value("${version}")
+    private String version;
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
         try {
+            Main mainApp = context.getBean(Main.class);
             GameServer gameServer = context.getBean(GameServer.class);
             gameServer.setConnectionLostTimeout(CONNECTION_LOST_TIMEOUT);
-            String version = loadVersion();
-            log.info("Starting ThatSoftwareGame server version {}", version);
+            log.info("Starting ThatSoftwareGame server version {}", mainApp.version);
             gameServer.run();
-        } catch (IOException e) {
-            log.error("Error loading version: {}", e.getMessage());
         } finally {
             log.info("Shutting down ThatSoftwareGame server");
             SpringApplication.exit(context, () -> 0);
         }
-    }
-
-    private static String loadVersion() throws IOException {
-        Properties properties = new Properties();
-        properties.load(Main.class.getClassLoader().getResourceAsStream("application.properties"));
-        return properties.getProperty("version");
     }
 }
